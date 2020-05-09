1756eb9ca62d694d8664c19e59fefb8ba58b2406
chris wang <wangguanzzz@gmail.com>
2020-05-09 19:59:22 +0800

    upddate

---
+++ run_transitgateway2.rb
@@ -0,0 +1,22 @@
+
+duration = 180
+speeds = [16000]
+lengths = [200]
+speeds.each do | speed|
+lengths.each do | length|
+    send_limit = (duration*speed*1.2).to_i
+    effective = duration*speed
+    limit = (effective*1.1).to_i
+    text = 'a'* length
+    #-effective {{ effective }} -limit {{ limit }}
+
+    receiver_string = "nohup ansible-playbook -i ansible/hosts_transitgateway2 ansible/01_receiver.yml --extra-vars \"effective=#{effective} limit=#{limit} group=224.1.1.2 \" > result2_#{speed}_#{length}.txt &"
+    # -text {{ playload }} -limit {{ limit }} -P {{ speed }} 
+    sender_string = "ansible-playbook -i ansible/hosts_transitgateway2 ansible/02_sender.yml --extra-vars \"playload=#{text} limit=#{send_limit} speed=#{speed} group=224.1.1.2 \""
+
+    system(receiver_string)
+    sleep 5
+    system(sender_string)
+    sleep 5
+end
+end
